09aee97c80d7ab88b29e435e2eee6b33673eae1f
Jerry Tong <zihangtong@gmail.com>
2022-01-26 00:54:18 -0500

    beautiful DnD implementation

--- notes-app-ui/src/Components/Folder.js
+++ notes-app-ui/src/Components/Folder.js
@@ -11,32 +11,31 @@ const Folder = ({
     setActiveNote
 }) => {
 	return (
-				<Accordion defaultActiveKey="0">
-				<Accordion.Item>
-					<Accordion.Header>{folder.name}</Accordion.Header>
-					<Accordion.Body>
-					{notes.map((note, i) => {
-						if (note.folder_id == folder.id) {
-							return (
-								<Draggable key={note.id} draggableId={note.id.toString()} index={i}>
-									{(provided) => (
-										<div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-											<Note
-												note={note}
-												onDeleteNote={onDeleteNote}
-												activeNote={activeNote}
-												setActiveNote={setActiveNote}
-											/>
-											{/* {provided.placeholder} */}
-										</div>
-									)}
-								</Draggable>
-							)
-						}
-					})}
-					</Accordion.Body>
-				</Accordion.Item>
-				</Accordion>
+		<Accordion defaultActiveKey="0">
+		<Accordion.Item>
+			<Accordion.Header>{folder.name}</Accordion.Header>
+			<Accordion.Body>
+			{notes.map((note, i) => {
+				if (note.folder_id == folder.id) {
+					return (
+						<Draggable key={note.id} draggableId={note.id.toString()} index={i}>
+							{(provided) => (
+								<div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+									<Note
+										note={note}
+										onDeleteNote={onDeleteNote}
+										activeNote={activeNote}
+										setActiveNote={setActiveNote}
+									/>
+								</div>
+							)}
+						</Draggable>
+					)
+				}
+			})}
+			</Accordion.Body>
+		</Accordion.Item>
+		</Accordion>
 	)
 }
 
